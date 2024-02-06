088f0dc9eb165a4d820bb63ad1dabcc9ca8d4f33
umairniaziofficial <umairniaziofficial@gmail..com>
2024-02-06 16:58:15 +0500

    Fixed Valentine Day Code

--- Project #33 Valentine/script.js
+++ Project #33 Valentine/script.js
@@ -1,6 +1,7 @@
 const yesBtn = document.getElementById("yes");
 const noBtn = document.getElementById("no");
-const mochiImg = document.getElementById("mochi");
+const image1 = document.getElementById("img1");
+const image2 = document.getElementById("img2");
 const ValentineText = document.getElementById("theText");
 
 let fontSize = 32; 
@@ -11,11 +12,16 @@ noBtn.addEventListener("click", () => {
 });
 
 
-yesBtn.addEventListener("click", () => {
+ yesBtn.addEventListener("click", () => {
    noBtn.style.display = "none";
    yesBtn.style.display = "none";
+   image1.style.display = "none";
+   
+   setTimeout(() => {
+    image2.style.display = "block";
    ValentineText.innerHTML = "Get set, Valentine! Our love journey is about to take off!";
-    mochiImg.src = "/elements/GifImage2.gif"
 
+   }, 150);
+   
 });
 
