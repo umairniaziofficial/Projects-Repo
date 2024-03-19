614e2bf47ae709e168e9e659477b8a56429580a6
umairniaziofficial <umairniaziofficial@gmail.com>
2024-03-20 00:02:58 +0500

    Styling Fixed

--- Project #06 Countdown Timer/script.js
+++ Project #06 Countdown Timer/script.js
@@ -1,23 +1,37 @@
-document.addEventListener("DOMContentLoaded", () => {
-  var countdownTime = new Date("2025-01-01T00:00:00").getTime();
+const viewEl = document.getElementById("view");
+viewEl.addEventListener("click", () => {
 
+  const dateEl = document.getElementById("thedate");
+  var selectedDate = new Date(dateEl.value);
+  
+  if (!dateEl.value) {
+    alert("Please enter a date first.");
+    return;
+  }
+  var today = new Date();
+  if (selectedDate < today) {
+    alert("Please select a date equal to or later than today.");
+    return;
+  }
+  var countdownTime = selectedDate.getTime();
   var x = setInterval(function () {
-    var today = new Date().getTime();
-
-    var distance = countdownTime- today;
+    var now = new Date().getTime();
+    var distance = countdownTime - now;
 
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    document.getElementById("timer").innerHTML=days + " days " + hours + " hours " + minutes + " minutes " + seconds+ " seconds";
+    document.getElementById("timer").innerHTML =
+      days + ` <span class='days'>${days>1?"days":"day"}</span> ` +
+      hours + ` <span class='hours'>${hours>1?"hours":"hour"}</span> ` +
+      minutes + ` <span class='minutes'>${minutes>1?"minutes":"minute"}</span> ` +
+      seconds + `<span class='seconds'>${seconds>1?" seconds":"second"}</span> `;
 
-    if(distance<0)
-    {
+    if (distance < 0) {
       clearInterval(x);
-      document.getElementById("timer").innerHTML="Time Reached";
+      document.getElementById("timer").innerHTML = "Time Reached";
     }
-
-  }, 1000)
+  }, 1000);
 });
