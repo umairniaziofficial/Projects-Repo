b20354c1e5d1c558e9766583e426cb85e1fa358a
umairniaziofficial <umairniaziofficial@gmail.com>
2024-03-19 13:34:49 +0500

    Fixed Ui/ux and fixed bugs and added functionality

--- Project #43 Random Choice Selector/script.js
+++ Project #43 Random Choice Selector/script.js
@@ -1,36 +1,55 @@
 const buttonsEl = document.getElementById("buttons");
 const textareaEl = document.getElementById("textarea");
+const messageEl = document.getElementById("msg");
+
+function selectRandomButton() {
+    var buttons = buttonsEl.getElementsByTagName("button");
+    var randomIndex = getRandomInt(buttons.length);
+    var iterations = 10;
+    var interval = setInterval(() => {
+        for (var i = 0; i < buttons.length; i++) {
+            buttons[i].style.backgroundColor = "black";
+        }
+        buttons[randomIndex].style.backgroundColor = "red";
+        randomIndex = getRandomInt(buttons.length);
+        iterations--;
+        if (iterations === 0) {
+            changeColor(buttons);
+            clearInterval(interval);
+            buttons[randomIndex].style.backgroundColor = "lightgreen";
+            buttons[randomIndex].style.color = "black";
+        }
+    }, 200);
+}
 
 textareaEl.addEventListener("keyup", (event) => {
     if (event.key === "Enter") {
-        var value = textareaEl.value.split(",");
+       const value =textareaEl.value.split(",").map(item =>item.trim()).filter(item=> item!="");
+       textareaEl.value = "";
         buttonsEl.innerHTML = "";
         for (var i = 0; i < value.length; i++) {
             var button = document.createElement("button");
             button.textContent = value[i].trim();
             buttonsEl.appendChild(button);
         }
+        messageEl.innerText = "Press Button for Another Random Choice!";
+        selectRandomButton();
     }
 });
+
+async function  changeColor(buttons)
+{
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].style.backgroundColor = "black";
+        buttons[i].style.color = "black";
+    }
+}
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
 
 buttonsEl.addEventListener("click", () => {
-    var buttons = buttonsEl.getElementsByTagName("button");
-    var randomIndex = getRandomInt(buttons.length);
-    var iterations = 10; 
-    var interval = setInterval(() => {
-        for (var i = 0; i < buttons.length; i++) {
-            buttons[i].style.backgroundColor = "white"; 
-        }
-        buttons[randomIndex].style.backgroundColor = "red"; 
-        randomIndex = getRandomInt(buttons.length); 
-        iterations--;
-        if (iterations === 0) {
-            clearInterval(interval);
-            buttons[randomIndex].style.backgroundColor = "lightgreen"; 
-        }
-    }, 200); 
+    
+    selectRandomButton();
 });
